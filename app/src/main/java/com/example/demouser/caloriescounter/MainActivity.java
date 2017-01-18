c2a4373cdd5a2538bde9c6a5e9399688b116087c
Jeanie Lim <lim24m@mtholyoke.edu>
2017-01-18 11:20:52 -0500

    implemented UI

--- app/src/main/java/com/example/demouser/caloriescounter/MainActivity.java
+++ app/src/main/java/com/example/demouser/caloriescounter/MainActivity.java
@@ -4,40 +4,57 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RadioButton noRestrictionButton;
+    private RadioButton VeganButton;
+    private RadioButton HalalButton;
+    private RadioButton NutFreeButton;
+    private CaloriesCounter counter;
+    private boolean isHalal, isVegan, isNoRestriction, isNutFree = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.noRestrictionButton) .setOnClickListener(new View.OnClickListener() {
+
+        noRestrictionButton = (RadioButton) findViewById(R.id.noRestrictionButton);
+        noRestrictionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                isNoRestriction = true;
             }
         });
 
-        findViewById(R.id.veganButton) .setOnClickListener(new View.OnClickListener() {
+        VeganButton = (RadioButton) findViewById(R.id.veganButton);
+        VeganButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                isVegan = true;
             }
         });
 
-        findViewById(R.id.halalButton) .setOnClickListener(new View.OnClickListener() {
+        HalalButton = (RadioButton) findViewById(R.id.halalButton);
+        HalalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                isHalal = true;
             }
         });
 
-        findViewById(R.id.noNutButton) .setOnClickListener(new View.OnClickListener() {
+        NutFreeButton = (RadioButton) findViewById(R.id.noNutButton);
+        NutFreeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                isNutFree = true;
             }
         });
+
+        counter = new CaloriesCounter(isNoRestriction, isVegan, isHalal, isNutFree);
+
     }
 }
